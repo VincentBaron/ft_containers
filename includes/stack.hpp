077f244415a7a90810e6ab34c7d979546b860c6d
VincentBaron <vincentbaron1996@gmail.com>
2022-02-09 21:46:02 +0000

    stack done

--- includes/stack.hpp
+++ includes/stack.hpp
@@ -6,13 +6,13 @@
 /*   By: vscode <vscode@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 16:53:22 by vscode            #+#    #+#             */
-/*   Updated: 2022/02/07 16:31:32 by vscode           ###   ########.fr       */
+/*   Updated: 2022/02/09 21:45:36 by vscode           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STACK_HPP
-# define STACK_HPP
-# include "vector.hpp"
+#define STACK_HPP
+#include "vector.hpp"
 
 namespace ft
 {
@@ -21,15 +21,58 @@ namespace ft
 	{
 	public:
 		typedef Container container_type;
-		typedef typename Container::size_type size_type;
-		
-		explicit stack(const container_type& ctnr = container_type()) : _ctnr(ctnr) {};
+		typedef T value_type;
+		typedef size_t size_type;
 
-		virtual ~stack() {};
+		explicit stack(const container_type &ctnr = container_type()) : _ctnr(ctnr){};
 
-		bool empty() const {return (_ctnr.empty());};
-		size_type size() const {return (_ctnr.size());};
+		virtual ~stack(){};
 
+		bool empty() const { return (_ctnr.empty()); };
+		size_type size() const { return (_ctnr.size()); };
+
+		value_type &top() { return _ctnr.back(); };
+		const value_type &top() const { return _ctnr.back(); };
+
+		void push(const value_type &val) { _ctnr.push_back(val); };
+
+		void pop(void) { _ctnr.pop_back(); };
+
+		template <class Tx, class Containerx>
+		friend bool operator==(const stack<Tx, Containerx> &lhs, const stack<Tx, Containerx> &rhs)
+		{
+			return (lhs._ctnr == rhs._ctnr);
+		};
+
+		template <class Tx, class Containerx>
+		friend bool operator!=(const stack<Tx, Containerx> &lhs, const stack<Tx, Containerx> &rhs)
+		{
+			return (lhs._ctnr != rhs._ctnr);
+		};
+
+		template <class Tx, class Containerx>
+		friend bool operator<(const stack<Tx, Containerx> &lhs, const stack<Tx, Containerx> &rhs)
+		{
+			return (lhs._ctnr < rhs._ctnr);
+		};
+
+		template <class Tx, class Containerx>
+		friend bool operator<=(const stack<Tx, Containerx> &lhs, const stack<Tx, Containerx> &rhs)
+		{
+			return (lhs._ctnr <= rhs._ctnr);
+		};
+
+		template <class Tx, class Containerx>
+		friend bool operator>(const stack<Tx, Containerx> &lhs, const stack<Tx, Containerx> &rhs)
+		{
+			return (lhs._ctnr > rhs._ctnr);
+		};
+
+		template <class Tx, class Containerx>
+		friend bool operator>=(const stack<Tx, Containerx> &lhs, const stack<Tx, Containerx> &rhs)
+		{
+			return (lhs._ctnr >= rhs._ctnr);
+		};
 
 	private:
 		container_type _ctnr;
